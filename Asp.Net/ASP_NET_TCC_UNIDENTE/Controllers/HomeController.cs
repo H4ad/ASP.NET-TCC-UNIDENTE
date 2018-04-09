29eabad2f2860f150f75a14fb6a1df6820175cc6
Vinicius Lourenço <vinyygamerlol@gmail.com>
2018-04-08 22:48:18 -0300

    MVC
    Criação do site em MVC.

---
+++ Asp.Net/ASP_NET_TCC_UNIDENTE/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_TCC_UNIDENTE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
